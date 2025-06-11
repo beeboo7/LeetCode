77bb4f10c77113b5bdfd43abc273afcc470bc3ec
Aimee Teppo <aimee.teppo@gmail.com>
2025-06-11 11:42:09 +1000

    Time: 14 ms (73.41%) | Memory: 12.4 MB (83.22%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        result = []
+
+        max_length = max(len(word1), len(word2))
+
+        for i in range(max_length):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+        return "".join(result)
